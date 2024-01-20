f5c5a7d0b5d528bc62a159767ca815363cdf4de2
Avinash Singh <avinashsingh@macbook-air.mynetworksettings.com>
2024-01-20 08:20:17 -0500

    trying basic app

--- backend/index.js
+++ backend/index.js
@@ -1,66 +1,11 @@
-import express from "express";
-import dotenv from "dotenv";
-import http  from 'http';
-import { faker } from '@faker-js/faker';
-import { ExpressPeerServer} from 'peer';
-import cors from "cors";
-
-dotenv.config();
-
+const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
-//http server running on port 3000
-const server = http.createServer(app);
-// //web socket on same port
-// const io = new Server(server);
 
-const peerServer = ExpressPeerServer(server, {
+app.get('/api/hello', (req, res) => {
+  res.send('Hello World!');
 });
 
-app.use("/peerjs", peerServer);
-app.use(cors());
-
-let users = new Map;
-
-app.get('/', (req, res) => {
-  res.send({
-    health: "200",
-  });
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
-
-app.get('/users/:id', (req, res) => {
-  const userId = req.params.id;
-  const usersList = Array.from(users.keys()).filter((id) => id != userId);
-  res.send({
-    users: usersList.map((id) => users.get(id)),
-  });
-});
-
-app.get('/getUserId/:user', (req, res) => {
-  const userName = req.params.user;
-  const userId = Array.from(users.keys()).filter((id) => users.get(id) == userName);
-  res.send({
-    userId: userId,
-  });
-});
-
-function generateRandomName() {
-  return faker.person.fullName(); // Generates a random name
-}
-
-peerServer.on('connection', (client) => { 
-  console.log("new user connected", client.getId()) 
-  users.set(client.getId(),generateRandomName());
-});
-
-peerServer.on('disconnect', (client) => {
-  console.log("user got disconnected");
-  users.delete(client.getId());
-});
-
-
-server.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
-
-export default app;
---
+++ backend/src/index.js
@@ -0,0 +1,66 @@
+import express from "express";
+import dotenv from "dotenv";
+import http  from 'http';
+import { faker } from '@faker-js/faker';
+import { ExpressPeerServer} from 'peer';
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+//http server running on port 3000
+const server = http.createServer(app);
+// //web socket on same port
+// const io = new Server(server);
+
+const peerServer = ExpressPeerServer(server, {
+});
+
+app.use("/peerjs", peerServer);
+app.use(cors());
+
+let users = new Map;
+
+app.get('/', (req, res) => {
+  res.send({
+    health: "200",
+  });
+});
+
+app.get('/users/:id', (req, res) => {
+  const userId = req.params.id;
+  const usersList = Array.from(users.keys()).filter((id) => id != userId);
+  res.send({
+    users: usersList.map((id) => users.get(id)),
+  });
+});
+
+app.get('/getUserId/:user', (req, res) => {
+  const userName = req.params.user;
+  const userId = Array.from(users.keys()).filter((id) => users.get(id) == userName);
+  res.send({
+    userId: userId,
+  });
+});
+
+function generateRandomName() {
+  return faker.person.fullName(); // Generates a random name
+}
+
+peerServer.on('connection', (client) => { 
+  console.log("new user connected", client.getId()) 
+  users.set(client.getId(),generateRandomName());
+});
+
+peerServer.on('disconnect', (client) => {
+  console.log("user got disconnected");
+  users.delete(client.getId());
+});
+
+
+server.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
+
+export default app;
